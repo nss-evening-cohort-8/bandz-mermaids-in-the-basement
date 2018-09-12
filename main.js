f4b2782d497eabeb290f5c6b895c3359bfbe35ca
Shane D Wilson <me@shanedwilson.com>
2018-09-12 17:33:07 -0500

    wip

--- main.js
+++ main.js
@@ -1,4 +1,59 @@
+let band = [
+  {
+    name: 'member 1',
+    image: 'https://orig00.deviantart.net/ab4b/f/2013/126/1/a/merman_by_annemaria48-d642or4.jpg',
+    bio: 'blah, blah, blah'
+  },
+
+  {
+    name: '',
+    image: '',
+    bio: ''
+  },
+
+  {
+    name: '',
+    image: '',
+    bio: ''
+  },
+
+  {
+    name: '',
+    image: '',
+    bio: ''
+  },
+
+  {
+    name: '',
+    image: '',
+    bio: ''
+  },
+
+  {
+    name: '',
+    image: '',
+    bio: ''
+  }
+]
+
+
 const printToDom = (stringToPrint, divId) => {
     const selectedDiv = document.getElementById(divId);
     selectedDiv.innerHTML += stringToPrint;
   };
+
+  const createBandCards = () => {
+    let newString = ''
+    for (let i = 0; i < band.length; i++) {
+        newString += `<div class="member">`
+          newString += `<h4>${band[i].name}</h4>`
+            newString += `<img src=${band[i].image}></img>`
+            newString +=`<div class="band__bio"`
+              newString += `<p>${band[i].bio}</p>`
+            newString += `</div>`  
+        newString += `</div>`;
+      };
+      printToDom(newString, 'bio__card');
+    };
+
+createBandCards();   

4d46a1b5891dea6b7396c3183a02ff6ade8d5e6a
Shane D Wilson <me@shanedwilson.com>
2018-09-12 17:46:22 -0500

    added image urls

--- main.js
+++ main.js
@@ -1,40 +1,40 @@
 let band = [
   {
-    name: 'member 1',
+    name: 'Aegaeon',
     image: 'https://orig00.deviantart.net/ab4b/f/2013/126/1/a/merman_by_annemaria48-d642or4.jpg',
     bio: 'blah, blah, blah'
   },
 
   {
-    name: '',
-    image: '',
-    bio: ''
+    name: 'Ladon',
+    image: 'https://i.pinimg.com/originals/16/3d/95/163d95f8b269f47ee0cfca91934dff57.jpg',
+    bio: 'blah, blah, blah'
   },
 
   {
-    name: '',
-    image: '',
-    bio: ''
+    name: 'Nereus',
+    image: 'https://cache.desktopnexus.com/thumbseg/458/458642-bigthumbnail.jpg',
+    bio: 'blah, blah, blah'
   },
 
   {
-    name: '',
-    image: '',
-    bio: ''
+    name: 'Pan',
+    image: 'https://i.warosu.org/data/tg/img/0294/29/1389487235150.png',
+    bio: 'blah, blah, blah'
   },
 
   {
-    name: '',
-    image: '',
-    bio: ''
+    name: 'Triton',
+    image: 'https://ssl.c.photoshelter.com/img-get2/I0000Xu80bDWLL6c/fit=1000x750/Project-Mermaids-78.jpg',
+    bio: 'blah, blah, blah'
   },
 
   {
-    name: '',
-    image: '',
-    bio: ''
+    name: 'Proteus',
+    image: 'http://mysticinvestigations.com/paranormal/wp-content/uploads/2013/08/merman1.jpg',
+    bio: 'blah, blah, blah'
   }
-]
+];
 
 
 const printToDom = (stringToPrint, divId) => {

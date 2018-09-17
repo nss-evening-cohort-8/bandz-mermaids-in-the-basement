96e39e209f09eaaeafb85945f1ef10d4409385a8
Matthew Jackson <mattshiloh@gmail.com>
2018-09-16 20:02:59 -0500

    removed a concert venue

--- main.js
+++ main.js
@@ -82,11 +82,6 @@ const tour = [
   when: "June 21, 2019&nbsp&nbsp&nbsp&nbsp",
   venue: "Harbor Cove Hotel in Nassau, Bahamas&nbsp&nbsp&nbsp&nbsp",
   time: "5 pm",
-},
-{
-  when: "July 16, 2019&nbsp&nbsp&nbsp&nbsp",
-  venue: "Civic Center in Amarillo, Texas&nbsp&nbsp&nbsp&nbsp",
-  time: "8 pm",
 }
 ];
 
